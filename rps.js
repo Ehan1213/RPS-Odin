eacb69dd1bd047dd1b93e4ab5e3d8d04adba7432
Ian L <187871945+Ehan1213@users.noreply.github.com>
2026-01-28 23:58:34 -0500

    Refactor RPS to allow for DOM manipulation and UI

--- rps.js
+++ rps.js
@@ -21,76 +21,139 @@ function getComputerChoice(){
 }
 
 
-const score = document.querySelector("score");
-const results = document.querySelector("roundResults");
-const playerScore = document.querySelector("playerScore");
-const computerScore = document.querySelector("computerScore");
+const score = document.querySelector(".score");
+const results = document.querySelector(".roundResults");
+const scoreBox = document.querySelector("score");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+
 
 
 const rockButton = document.querySelector("#rockButton")
 rockButton.addEventListener("click", () => {
     const humanChoice = "rock";
-    alert('You pushed rock');
-    return humanChoice;
+    // alert('You pushed rock');
+    playRound(humanChoice)
 });
 
 const paperButton = document.querySelector("#paperButton")
 paperButton.addEventListener("click", () => {
     const humanChoice = "paper";
-    alert('You pushed paper');
-    return humanChoice;
+    // alert('You pushed paper');
+    playRound(humanChoice);
 });
 
 const scissorsButton = document.querySelector("#scissorsButton")
 scissorsButton.addEventListener("click", () => {
     const humanChoice = "scissors";
-    alert('You pushed scissors');
-    return humanChoice;
+    // alert('You pushed scissors');
+    playRound(humanChoice);
 });
-
-
 
 
 
 let playerTotal = 0;
 let computerTotal = 0;
+playerScore.textContent += `Player: ${playerTotal}`
+computerScore.textContent += `Computer: ${computerTotal}`
+
 
 
 function playRound(humanChoice){
-    let   humanSelection = humanChoice;
-    let   computerSelection = getComputerChoice();
+    let computerSelection = getComputerChoice();
+    let humanSelection = humanChoice;
 
     // tie logic block
-    if (humanSelection == computerSelection){ 
+    if (humanSelection == computerSelection){
+        computerTotal += 1;
+        playerTotal += 1;
+        computerScore.textContent = `Computer: ${computerTotal}`
+        playerScore.textContent = `Player: ${playerTotal}`
 
-        console.log("It's a tie! " + humanSelection + " and " + computerSelection + " are the same!");
+
+        const resultItem = document.createElement("li");
+
+        // debugging
+        // console.log(`It's a tie! ${humanSelection} and ${computerSelection} are the same!`)
+
+        resultItem.textContent = `It's a Tie! ${humanSelection} and ${computerSelection} are the same!`;
+        results.appendChild(resultItem);
     }
 
     // rock and paper logic block
     if (humanSelection == "rock" && computerSelection == "paper"){
         computerTotal += 1;
-        console.log("You lose! " + computerSelection + " beats " + humanSelection);
+        computerScore.textContent = `Computer: ${computerTotal}`
+
+        // debugging
+        // console.log(`You lost! ${humanSelection} lost to ${computerSelection}`)
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You lost! ${humanSelection} lost to ${computerSelection}`;
+        results.appendChild(resultItem);
+
     } else if (humanSelection == "paper" && computerSelection == "rock"){
         playerTotal += 1;
-        console.log("You Win! " + humanSelection + " beats " + computerSelection);
+        playerScore.textContent = `Player: ${playerTotal}`
+
+        // debugging
+        // console.log("You Win! " + humanSelection + " beats " + computerSelection);
+
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You Win! ${humanSelection} beats ${computerSelection}`;
+        results.appendChild(resultItem);
     }
+
 
     // rock and scissors logic block
     if (humanSelection == "scissors" && computerSelection == "rock"){
         computerTotal += 1;
-        console.log("You lose! " + computerSelection + " beats " + humanSelection);
+        computerScore.textContent = `Computer: ${computerTotal}`
+
+        // debugging
+        // console.log(`You lost! ${humanSelection} lost to ${computerSelection}`)
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You lost! ${humanSelection} lost to ${computerSelection}`;
+        results.appendChild(resultItem);
+        
     } else if (humanSelection == "rock" && computerSelection == "scissors"){
         playerTotal += 1;
-        console.log("You Win! " + humanSelection + " beats " + computerSelection);
+        playerScore.textContent = `Player: ${playerTotal}`
+
+        // debugging
+        // console.log("You Win! " + humanSelection + " beats " + computerSelection);
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You Win! ${humanSelection} beats ${computerSelection}`;
+        results.appendChild(resultItem);
     }
         
     // paper and scissors logic block
     if (humanSelection == "paper" && computerSelection == "scissors"){
         computerTotal += 1;
-        console.log("You lose! " + computerSelection + " beats " + humanSelection);
+        computerScore.textContent = `Computer: ${computerTotal}`
+
+
+        // debugging
+        // console.log(`You lost! ${humanSelection} lost to ${computerSelection}`)
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You lost! ${humanSelection} lost to ${computerSelection}`;
+        results.appendChild(resultItem);
+
     } else if (humanSelection == "scissors" && computerSelection == "paper"){
         playerTotal += 1;
-        console.log("You Win! " + humanSelection + " beats " + computerSelection);
+        playerScore.textContent = `Player: ${playerTotal}`
+
+
+        // debugging
+        // console.log("You Win! " + humanSelection + " beats " + computerSelection);
+
+        const resultItem = document.createElement("li");
+        resultItem.textContent = `You Win! ${humanSelection} beats ${computerSelection}`;
+        results.appendChild(resultItem);
     }
 }
 
